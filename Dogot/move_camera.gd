c51b6df82184de9409e6741dbf0f0e274ebb7f8d
Плотников Михаил <pam@mail.ru>
2024-02-18 21:34:18 +0300

    Движение камеры как вдоте через мидл мыш кнопка

--- Dogot/move_camera.gd
+++ Dogot/move_camera.gd
@@ -1,6 +1,8 @@
 extends Camera
 
 var movement_speed = 10.0
+var is_mouse_dragging = false
+var prev_mouse_pos = Vector2()
 
 func _process(delta):
 	# Получаем вектор направления от клавиш WASD
@@ -20,3 +22,15 @@ func _process(delta):
 
 	# Перемещаем камеру
 	translate(movement_vector * movement_speed * delta)
+
+	# Обрабатываем движение мыши по зажатию средней кнопки
+	if Input.is_action_pressed("midle_mouse"):
+		if not is_mouse_dragging:
+			is_mouse_dragging = true
+			prev_mouse_pos = get_viewport().get_mouse_position()
+		else:
+			var delta_mouse = get_viewport().get_mouse_position() - prev_mouse_pos
+			translate(Vector3(-delta_mouse.x, delta_mouse.y, 0) * movement_speed * delta)
+			prev_mouse_pos = get_viewport().get_mouse_position()
+	else:
+		is_mouse_dragging = false
