f80f6f908fe04225a8489bd89372e1d6f8784483
Плотников Михаил <pam@mail.ru>
2024-02-19 19:17:01 +0300

    Функция инициализации персонажа

--- Dogot/inventary.gd
+++ Dogot/inventary.gd
@@ -1,16 +1,34 @@
-extends Node
+"""
+ИНВЕНТАРЮ
 
+Как выгледит инвентарь:
+	
+				Головной убор
+				Плечи: наплечники, плащ
+	Оружие		Туловище(и не только ещё ноги и руки это один слот, ПОКА): броня, мантии			Оружие:
+				Ботинки
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+"""
 
+#var inventary = preload("res://inventary.gd")  создание объекта этого класса
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var head = null
 
+var shoulders = null
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var weapon = null
+
+var torso = null
+
+var shoes = null # обувь
+
+func _init(head_, shoulders_, weapon_, torso_, shoes_):
+	var head =  head_
+
+	var shoulders = shoulders_
+
+	var weapon = weapon_
+
+	var torso = torso_
+
+	var shoes = shoes_ # обувь
--- Dogot/person.gd
+++ Dogot/person.gd
@@ -1,16 +1,74 @@
-extends Node
+#Класс всей информации о персонаже
+#Основные переменные
 
+var class_person = null # класс: ближник, дальник, маг и тд 
+"""
+Краткий экскур по классам:
+	
+	Паладин: ближник, танк, рыцарь.
+		Имеете max_hp = 1000
+			max_mana = 200
+			regen_hp = 10
+			regen_mana = 1
+			armor = 10
+			mag_resist = 30
+			damage = 80
+			attack_speed = 2
+			attack_radius = хз в чём измерять но примерно вытянутая рука
+			speed = средняя скорость
+		Мб вы скажите что он сильный даже по рамкам доты но у него не будет очивидных обилок. Те если он будет покупать вещи дающие обилки он сможет колдаватьт
+		
+		
+	Стрелок: лучник, чучело с луком, пкм герой.
+		Имеете max_hp = 600
+			max_mana = 500
+			regen_hp = 4
+			regen_mana = 4
+			armor = 2
+			mag_resist = 15
+			damage = 75
+			attack_speed = 1,5
+			attack_radius = в 3 раза больше чем у паладина
+			speed = быстрый
+			
+			
+	Маг: пионист.
+		Имеете max_hp = 400
+			max_mana = 700
+			regen_hp = 2
+			regen_mana = 10
+			armor = 0
+			mag_resist = 50
+			damage = 100
+			attack_speed = 0,5
+			attack_radius = в 4 раза больше чем у паладина
+			speed = медленный
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+"""
+# null сдешний None
+var max_hp = null
+var hp = max_hp
 
+var max_mana = null
+var mana = max_mana
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var regen_hp = null
+var regen_mana = null
 
+var armor = null # это то насколько уменьшается физ урон. Допустим урон у врага с руки 100 он попадает по тебу кагда у тебя 20 брони и наносит 100-20 = 80
+var mag_resist = null  # это процен блокировки магического урона. Допустим урон у врага магией 100 он попадает по тебу кагда у тебя 30 процентов резист и наносит 100-100*0.3 = 70
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var damage = null
+var attack_speed = null # Измеряется в за сколько секунд один удар
+var attack_radius = null
+var speed = null # ещё хз в чём измерять
+
+# Все вышеперечисленные характеристики это те что могут меняться и будут изменены в начале игры при выборе класса и оружия
+
+var xp = 0
+var lvl = 1 # думаю сделаем как в большинстве РПГ игр xp впрогрессии увеличивается, только надо найти золотую серидину
+
+var inventary = null
+
+func _init(class_person, inventory):
+	
