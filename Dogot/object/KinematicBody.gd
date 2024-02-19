61befd02d0e38c29c6008561887b62cf5e97f19b
Плотников Михаил <pam@mail.ru>
2024-02-19 17:40:14 +0300

    Починил баг с тем что при нажатии на персонажа он поднимался

--- Dogot/object/KinematicBody.gd
+++ Dogot/object/KinematicBody.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 var target_position = Vector3.ZERO
 var speed = 6
+var rotation_threshold = 0.1
+var rotation_speed = 5
 
 func _process(delta):
 	# Обработка движения персонажа
@@ -10,11 +12,12 @@ func _process(delta):
 	# Перемещение к целевой позиции
 	if target_position != Vector3.ZERO:
 		var direction = (target_position - translation).normalized()
+		direction.y = 0
 		move_and_slide(direction * speed)
 
 func _input(event):
 	# Обработка ввода от игрока
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and Input.is_action_pressed("right_click"):
 		# Получение позиции, на которую нажал игрок
 		var mouse_position = event.position
 		var clicked_point = get_viewport().get_camera().project_ray_origin(mouse_position)
@@ -26,5 +29,4 @@ func _input(event):
 		if result:
 			target_position = result.position
 			print(target_position)
-
 
