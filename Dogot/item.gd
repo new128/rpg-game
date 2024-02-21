4f7b9196763d33093a1d73d9a1fbe852d0b26870
Плотников Михаил <pam@mail.ru>
2024-02-21 16:37:45 +0300

    Добавил инит айтему и отображение инвентаря

--- Dogot/item.gd
+++ Dogot/item.gd
@@ -8,3 +8,9 @@
 var characteristic = {} #  в словаре будет характеристика типо макс хп и значение изменения её
 var name = null
 var description = null
+
+
+func _init(characteristic_, name_, description_):
+	characteristic = characteristic_
+	name = name_
+	description = description_
