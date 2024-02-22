aa6119e2eeebc782ea7c2b56b60f8e638c5e6be4
Плотников Михаил <pam@mail.ru>
2024-02-22 17:28:06 +0300

    Что то похожее на магазин

--- Dogot/Control.gd
+++ Dogot/Control.gd
@@ -5,6 +5,7 @@ var ac_d3 = true
 var ac_d4 = true
 var ac_d5 = true
 
+
 func _ready():
 	$AcceptDialog.popup()
 
@@ -41,3 +42,7 @@ func _process(delta):
 	if not $AcceptDialog4.visible and not ac_d3 and not ac_d2 and not ac_d4 and ac_d5:
 		$AcceptDialog5.popup()
 		ac_d5 =false
+		
+func _input(event):
+	if event is InputEventKey and event.pressed and Input.is_action_pressed("f4"):
+		$Shop.visible = not $Shop.visible 
